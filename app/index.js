8549caaa30b76ce7bf3ad3bcd21fad02c0d96b71
Daisy Dai <893487287@qq.com>
2018-04-04 20:31:26 +0800

    Import the third part modul

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 require('./sass/main.scss')
+
+var $ = require('jquery');
 var sub = require('./sub');
+var moment = require('moment');
 var app = document.createElement('div');
 
 app.innerHTML = '<h1>Hello Word</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
+$('body').append('<p>look at me! now is ' + moment().format() + '</p>')
